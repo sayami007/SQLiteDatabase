57dbec88eea227d83a8f2f4dfad123837310cd29
Bibesh <bibesh.manandhar@gmail.com>
2016-11-22 07:53:17 +0545

    Changed the Constant object into static and also created DBHelper class

--- app/src/main/java/application/unicef/com/sqlitedatabase/DbConstant.java
+++ app/src/main/java/application/unicef/com/sqlitedatabase/DbConstant.java
@@ -8,10 +8,10 @@ import android.renderscript.ScriptIntrinsicYuvToRGB;
 
 public class DbConstant {
 
-    public int VERSION_CODE=1;
-    public String DB_NAME="student";
-    public String DB_TABLE="student_table";
-    public String CREATE_QUERY="CREATE TABLE "+DB_TABLE+" person varchar(255)";
-    public String DELETE_QUERY="DROP TABLE IF EXISTS "+DB_NAME;
+    public static int VERSION_CODE=1;
+    public static String DB_NAME="student";
+    public static String DB_TABLE="student_table";
+    public static String CREATE_QUERY="CREATE TABLE "+DB_TABLE+" person varchar(255)";
+    public static String DELETE_QUERY="DROP TABLE IF EXISTS "+DB_NAME;
 
 }
--- app/src/main/java/application/unicef/com/sqlitedatabase/DbHelper.java
+++ app/src/main/java/application/unicef/com/sqlitedatabase/DbHelper.java
@@ -1,5 +1,6 @@
 package application.unicef.com.sqlitedatabase;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -7,8 +8,20 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Bibesh on 11/21/16.
  */
 
-public class DbHelper {
-    int _id;
-    String _name;
-    String _phone_number;
+public class DbHelper extends SQLiteOpenHelper {
 
+    public DbHelper(Context context) {
+        super(context, DbConstant.DB_NAME, null, DbConstant.VERSION_CODE);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(DbConstant.CREATE_QUERY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(DbConstant.DELETE_QUERY);
+        onCreate(sqLiteDatabase);
+    }
+}
